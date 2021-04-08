260d7c0f679bf88132e73fb5e46376552d44e4f7
Benjamin Yolken <benjamin.yolken@segment.com>
2021-04-07 18:29:55 -0700

    Update fixtures

--- _fixtures/chained_calls__exp.go
+++ _fixtures/chained_calls__exp.go
@@ -14,27 +14,16 @@ func NewChain() *Chain {
 
 func ChainedCalls() {
 	c := Chain{}
-	c.ChainCall(
-		"a long argument",
-		"another long argument",
-		"a third long argument",
-	).ChainCall(
-		"a long argument2",
-		"another long argument2",
-		"a third long argument2",
-	).ChainCall(
-		"a long argument3",
-		"another long argument3",
-		"a third long argument3",
-	)
+	c.ChainCall("a long argument", "another long argument", "a third long argument").
+		ChainCall("a long argument2", "another long argument2", "a third long argument2").
+		ChainCall("a long argument3", "another long argument3", "a third long argument3")
 	NewChain().ChainCall(
 		"a really really really really really long argument4",
 		"another really really really really really long argument4",
-		fmt.Sprintf(
-			"%v",
-			"this is a long method",
-		),
-	).ChainCall("a really really really really really long argument5", "another really really really really really long argument5", "a third really really really really really long argument5").ChainCall("a", "b", fmt.Sprintf("%v", "this is a long method"))
+		fmt.Sprintf("%v", "this is a long method"),
+	).
+		ChainCall("a really really really really really long argument5", "another really really really really really long argument5", "a third really really really really really long argument5").
+		ChainCall("a", "b", fmt.Sprintf("%v", "this is a long method"))
 	NewChain().ChainCall("a", "b", "c").ChainCall("d", "e", "f")
 	NewChain().ChainCall("a", "b", "c").
 		ChainCall("d", "e", "f").ChainCall("g", "h", "i")
